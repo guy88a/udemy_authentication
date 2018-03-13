f70f421d1bd71195d2f9f2162c0d536c12db089b
guyaston <guy.aston@themarker.com>
2018-03-13 16:20:18 +0200

    HOC S14L113

---
+++ client/components/Dashboard.js
@@ -0,0 +1,15 @@
+// IMPORTS ==================================================================//
+import React from "react";
+
+// DEFINE COMPONENT =========================================================//
+const Dashboard = props => {
+  return (
+    <div>
+      <p>You are loggen in!</p>
+      <h4>Dashboard</h4>
+    </div>
+  );
+};
+
+// EXPORT ===================================================================//
+export default Dashboard;
--- client/index.js
+++ client/index.js
@@ -9,6 +9,8 @@ import { Router, hashHistory, Route, IndexRoute } from "react-router";
 import App from "./components/App";
 import LoginForm from "./components/LoginForm";
 import SignupForm from "./components/SignupForm";
+import Dashboard from "./components/Dashboard";
+import requireAuth from "./components/requireAuth";
 
 // SETTINGS =================================================================//
 const networkInterface = createNetworkInterface({
@@ -31,6 +33,7 @@ const Root = () => {
         <Route path="/" component={App}>
           <Route path="login" component={LoginForm} />
           <Route path="signup" component={SignupForm} />
+          <Route path="dashboard" component={requireAuth(Dashboard)} />
         </Route>
       </Router>
     </ApolloProvider>
